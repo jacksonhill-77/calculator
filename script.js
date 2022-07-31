2b7d81851327476c8e1fd88fcd89bf4f88fbd996
jacksonreaction <contactjacksonhill@gmail.com>
2022-07-31 21:59:19 +1000

    Add variables to fix tracking of operators in calculations with >2 numbers

--- script.js
+++ script.js
@@ -3,32 +3,67 @@ let operators = document.querySelectorAll('.operator')
 let displayDiv = document.querySelector('.display-text')
 let clear = document.querySelector('.clear')
 let equals = document.querySelector('.equals')
-let previousNumber = "initial"
+let number1 = ""
+let number2 = ""
+let operator = ""
+let operatorCurrent = false
+let calculation = ""
 
-// Define event listeners for populating the display
+// When a number button is pressed, 
 for (n of numbers) {
-    n.addEventListener('click', (e) =>
-        displayDiv.textContent += e.target.textContent)
+    n.addEventListener('click', (e) => {
+        // check if an operator has already been clicked
+        if (operatorCurrent === true) {
+            // if it has, change the display to just this number, 
+            // clearing the previous display
+            displayDiv.textContent = e.target.textContent
+            operatorCurrent = false
+        } else {
+            // if it hasn't, keep adding the numbers to the
+            // existing display
+            displayDiv.textContent += e.target.textContent
+        }
+    })
 }
 
 for (o of operators) {
     o.addEventListener('click', (e) => {
-        if (previousNumber === "initial") {
-            previousNumber = Number(displayDiv.textContent)
-            displayDiv.textContent = e.target.textContent
+        // If no number has been entered yet
+        if (number1 === "") {
+            number1 = Number(displayDiv.textContent)
         } else {
-            previousNumber += Number(displayDiv.textContent)
-            displayDiv.textContent = e.target.textContent
+            number2 = Number(displayDiv.textContent)
+        }
+        // If this is the first time an operator is used
+        // in a sequence of calculations
+        if (operatorCurrent === false) {
+            // then the operator variable becomes the button just clicked
+            operator = e.target.textContent
+            operatorCurrent = true
+                // else if it is the 2nd or later time the operator
+                // is used
+        } else if (operator !== "") {
+            operator = e.target.textContent
+            console.log(operator, number1, number2)
+            calculation = Number(operate(operator, number1, number2))
+            console.log(calculation)
+            displayDiv.textContent = calculation
+            number1 = Number(displayDiv.textContent)
+            operatorCurrent = true
         }
     })
 }
 
 clear.addEventListener('click', () => {
     displayDiv.textContent = ""
+    number1 = ""
+    number2 = ""
+    operator = ""
+    calculation = ""
 })
 
 equals.addEventListener('click', () => {
-    displayDiv.textContent = ""
+    displayDiv.textContent = operate(operator, number1, number2)
 })
 
 // Define the basic calculating operators
@@ -64,13 +99,15 @@ const divide = function(...args) {
 };
 
 const operate = function(operator, number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
     if (operator === "+") {
         return (add(number1, number2))
     }
     if (operator === "-") {
         return (subtract(number1, number2))
     }
-    if (operator === "*") {
+    if (operator === "x") {
         return (multiply(number1, number2))
     }
     if (operator === "/") {
